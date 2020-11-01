6107395a7147d1af60630878df5700b38821ee1d
ALPOFF <leha.mms82@gmail.com>
2020-11-01 17:37:31 +0300

    reworked logic

--- main.js
+++ main.js
@@ -2,44 +2,78 @@
 
 class Calculator {
 	constructor() {
+		this.result = null;
+		this.express = ''
 		this.memory = {
-			express: '',
 			result: null
 		}
 	}
 
-	delete() {
-		this.memory.express = ''
+	resetCalc() {
+		console.log("in reset")
+		this.express = ''
+		this.result = null;
+		this.memory.result = null;
+	}
+
+	clearDisplay() {
+		displayInput.value = "0"
+		this.express = ''
+		this.result = null
 	}
 
 	compute() {
-		this.memory.express = eval(this.memory.express)
+		if (this.express === '') {
+			displayInput.value = "0"
+		} else {
+			this.express = eval(this.express)
+			this.result = parseInt(this.express)
+		}
 	}
 
 	appendNumber(number) {
-		this.memory.express += number
+		this.express += number
 	}
 
 	appendOperation(operation) {
 		operation === "x"
-			? this.memory.express += '*'
-			: this.memory.express += operation
+			? this.express += '*'
+			: this.express += operation
 	}
 
 	saveInMemory() {
-
+		this.memory.result = this.result
+		memoryIndicator.style.backgroundColor = 'green'
 	}
 
 	readFromMmory() {
-		this.memory.result
+		this.express += this.memory.result
 	}
 
 	clearMemory() {
 		this.memory.result = null
+		memoryIndicator.style.backgroundColor = 'red'
+	}
+
+	clearAC() {
+		displayInput.value = "0"
+		this.resetCalc()
+		memoryIndicator.style.backgroundColor = 'red'
 	}
 
 	updateDisplay() {
-		displayInput.value = this.memory.express
+		if (this.express == 'Infinity') {
+			displayInput.value="ERROR"
+			this.resetCalc()
+		} else {
+			displayInput.value = this.express
+		}
+	}
+
+	checkForFirst() {
+		if (this.express === '') {
+			displayInput.value = 0
+		}
 	}
 }
 
@@ -51,57 +85,55 @@ const allClearButton = document.querySelector('.data-all-clear')
 const memoryButton = document.querySelector('.data-memory')
 const memoryReadButton = document.querySelector('.data-memory-read')
 const memoryClearButton = document.querySelector('.data-memory-clear')
+const memoryIndicator = document.getElementById('memory-indicator')
 const displayInput = document.getElementById('display')
-
-
 const prevOperand = document.querySelector('.data-previous-operand')
 const curOperand = document.querySelector('.data-current-operand')
 
 const calculator = new Calculator()
 
 numberButtons.forEach(button => {
-
 	button.addEventListener('click', () => {
 		calculator.appendNumber(button.value)
 		calculator.updateDisplay()
 	})
 })
 
-operationButtons.forEach(button => {
 
-	button.addEventListener('click', () => {
-		calculator.appendOperation(button.value)
+if (calculator.express !== '') {
+	operationButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			calculator.appendOperation(button.value)
+			calculator.updateDisplay()
+		})
+	})
+	
+	equalButton.addEventListener('click', button => {
+		calculator.compute()
 		calculator.updateDisplay()
 	})
-})
-
-equalButton.addEventListener('click', button => {
-	calculator.compute()
-	calculator.updateDisplay()
-})
-
-allClearButton.addEventListener('click', button => {
-	calculator.clear()
-	calculator.updateDisplay()
-})
-
-deleteButton.addEventListener('click', button => {
-	calculator.delete()
-	calculator.updateDisplay()
-})
-
-memoryButton.addEventListener('click', button => {
-	calculator.saveInMemory()
-	calculator.updateDisplay()
-})
-
-memoryReadButton.addEventListener('click', button => {
-	calculator.readFromMmory()
-	calculator.updateDisplay()
-})
-
-memoryClearButton.addEventListener('click', button => {
-	calculator.clearMemory()
-	calculator.updateDisplay()
-})
+	
+	allClearButton.addEventListener('click', button => {
+		calculator.clearAC()
+	})
+	
+	deleteButton.addEventListener('click', button => {
+		calculator.clearDisplay()
+	})
+	
+	memoryButton.addEventListener('click', button => {
+		calculator.saveInMemory()
+		calculator.updateDisplay()
+	})
+	
+	memoryReadButton.addEventListener('click', button => {
+		calculator.readFromMmory()
+		calculator.updateDisplay()
+	})
+	
+	memoryClearButton.addEventListener('click', button => {
+		calculator.clearMemory()
+		calculator.updateDisplay()
+	})
+}
 
